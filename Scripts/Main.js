cf808bbc4ea31c50a99226f2c9858be7cb234a4d
Jake Blackmon <9832801@isd535.org>
2021-12-03 09:15:18 -0600

    fixed a typo on maoin.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -7,7 +7,7 @@ let App = window.App;
 let Checklist = App.Checklist;
 let Truck = App.Truck;
 let DataStore = App.DataStore;
-let RemoteDataStore = App.ReomteDataStore;
+let RemoteDataStore = App.RemoteDataStore;
 let FormHandler = App.FormHandler;
 let Validation = App.Validation 
 })(window);
