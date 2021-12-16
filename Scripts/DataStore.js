6c6dfc1818526666c52417aaafcdd3e41fa9d250
Jake Blackmon <9832801@isd535.org>
2021-12-16 08:57:50 -0600

    working on datastore

--- Scripts/DataStore.js
+++ Scripts/DataStore.js
@@ -4,9 +4,12 @@
     let App = window.App || {};
 
     function DataStore() {
-        console.log('running the DataStore function...');
         this.data = {};
     }
+
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+    };
 
     App.DataStore = DataStore;
     window.App = App;
