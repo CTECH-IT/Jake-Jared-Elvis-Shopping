588d801620cfff4c89271e9f106f49b59e3c4aa1
Elvis Vermilyea <9233802@isd535.org>
2021-12-17 09:09:07 -0600

    gamne

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -2,23 +2,26 @@
     'use strict';
 
     const FORM_SELECTOR = '[data-sushi-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-sushi-order="checklist"]';
     const Server_URL = 'http://saturn.rochesterschools.org:8080/json';
 
-let App = window.App;
-let Truck = App.Truck;
-let DataStore = App.DataStore;
-let Checklist = App.Checklist;
-let RemoteDataStore = App.RemoteDataStore;
-let FormHandler = App.FormHandler;
-let Validation = App.Validation; 
 
-let myTruck = new Truck('12345', new DataStore());
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+    let RemoteDataStore = App.RemoteDataStore;
+    let FormHandler = App.FormHandler;
+    let CheckList = App.Checklist;
+    let Validation = App.Validation;
 
-window.myTruck = myTruck
-formHandler.addInputHandler(Validation.isCompanyEmail);
 
-let formHandler = new FormHandler(FORM_SELECTOR);
-formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-console.log(formHandler);
+    let myTruck = new Truck('12345', new DataStore());
 
-})(window);s
+    window.myTruck = myTruck
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
+    let formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
+
+})(window); s
