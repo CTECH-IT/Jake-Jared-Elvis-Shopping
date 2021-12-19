591378ac700776f6d4b4edb53721f9d42b651b5d
elvis <9233802@isd535.org>
2021-12-19 04:46:14 -0600

    added sleep deprived notes for me to laugh at latr

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -31,3 +31,21 @@
     formHandler.addInputHandler(Validation.isCompanyEmail);
 
 })(window); 
+
+/*
+elvis's sleep deptrivation notes
+
+i have no idea how i actually got this working
+there were many small bugs that kept it from functioning
+suddenly it worked, but it didnt show any information other than email
+i realized that i needed to add different descriptions to the checklist
+and then changed the sides to a dropdown
+then added an amount dropdown
+then added both of those to the description
+and then i added every single one of the sushi checkboxes into the description inside of an if statement asking if the checkbox was selected
+after i got that eventually working i added a price to each of the checkboxes, and then made it add the prices of all sushi rolls together, then multiply it by the amount of rolls selected
+then added the final number to the checklist description
+i need sleep
+its 5 am
+whats that bright thing in the sky?
+*/
